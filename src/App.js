712f61e5f982b9c7efdd4216cb6a835fef559e87
Abay Ibrayev <abayibrayev7@gmail.com>
2021-09-13 22:09:06 +0100

    initial layout + project structure set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
+import React, { Fragment } from 'react';
+
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/header.component';
+import List from './components/List/list.component';
+import Map from './components/Map/map.component';
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <Fragment>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </Fragment>
+  );
 }
 
 export default App;
