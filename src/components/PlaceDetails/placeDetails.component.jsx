c8875dd74c93e414b5fb90231fb3c14c7c707a3e
Abay Ibrayev <abayibrayev7@gmail.com>
2021-09-16 00:26:09 +0100

    place details card completed

--- src/components/PlaceDetails/placeDetails.component.jsx
+++ src/components/PlaceDetails/placeDetails.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {
   Box,
   Typography,
@@ -17,47 +17,112 @@ import Rating from '@material-ui/lab';
 import useStyles from './placeDetails.styles';
 
 const PlaceDetails = ({ place }) => {
-  const { name, photo, price_level, ranking, awards } = place;
+  const {
+    name,
+    photo,
+    price_level,
+    ranking,
+    awards,
+    cuisine,
+    address,
+    phone,
+    distance_string,
+    web_url,
+    website,
+  } = place;
   const classes = useStyles();
   return (
-    <Card elevation={6}>
-      <CardMedia
-        style={{ height: 350 }}
-        image={
-          photo
-            ? photo.images.large.url
-            : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
-        }
-        title={name}
-      />
-      <CardContent>
-        <Typography gutterBottom variant='h6'>
-          {name}
-        </Typography>
-        <Box display='flex' justifyContent='space-between'>
-          <Typography variant='subtitle1'>Price</Typography>
-          <Typography gutterBottom variant='subtitle1'>
-            {price_level}
-          </Typography>
-        </Box>
-        <Box display='flex' justifyContent='space-between'>
-          <Typography variant='subtitle1'>Ranking</Typography>
-          <Typography gutterBottom variant='subtitle1'>
-            {ranking}
-          </Typography>
-        </Box>
-        {awards
-          ?.filter((item, index) => index < 3)
-          .map((award) => (
-            <Box my={1} display='flex' justifyContent='space-between'>
-              <img src={award.images.small} alt={award.display_name} />
-              <Typography variant='subtitle2' color='textSecondary'>
-                {award.display_name}
+    <Fragment>
+      {name && (
+        <Card elevation={6}>
+          <CardMedia
+            style={{ height: 350 }}
+            image={
+              photo
+                ? photo.images.large.url
+                : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+            }
+            title={name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h6'>
+              {name} ({distance_string})
+            </Typography>
+            <Box display='flex' justifyContent='space-between'>
+              <Typography variant='subtitle1'>Price</Typography>
+              <Typography gutterBottom variant='subtitle1'>
+                {price_level}
               </Typography>
             </Box>
-          ))}
-      </CardContent>
-    </Card>
+            <Box display='flex' justifyContent='space-between'>
+              <Typography variant='subtitle1'>Ranking</Typography>
+              <Typography gutterBottom variant='subtitle1'>
+                {ranking}
+              </Typography>
+            </Box>
+            {awards
+              ?.filter((item, index) => index < 3)
+              .map((award) => (
+                <Box my={1} display='flex' justifyContent='space-between'>
+                  <img src={award.images.small} alt={award.display_name} />
+                  <Typography variant='subtitle2' color='textSecondary'>
+                    {award.display_name}
+                  </Typography>
+                </Box>
+              ))}
+            {cuisine?.map(({ name }) => (
+              <Chip
+                key={name}
+                size='small'
+                label={name}
+                className={classes.chip}
+              />
+            ))}
+            {address && (
+              <Typography
+                gutterBottom
+                variant='subtitle2'
+                color='textSecondary'
+                className={classes.subtitle}
+              >
+                <LocationOnIcon /> {address}
+              </Typography>
+            )}
+            {phone && (
+              <Typography
+                gutterBottom
+                variant='subtitle2'
+                color='textSecondary'
+                className={classes.spacing}
+              >
+                <PhoneIcon /> {phone}
+              </Typography>
+            )}
+
+            <CardActions>
+              {web_url && (
+                <Button
+                  size='small'
+                  color='primary'
+                  onClick={() => window.open(web_url, '_blank')}
+                >
+                  Trip Advisor
+                </Button>
+              )}
+              {website && (
+                <Button
+                  size='small'
+                  color='primary'
+                  onClick={() => window.open(website, '_blank')}
+                >
+                  Website
+                </Button>
+              )}
+            </CardActions>
+          </CardContent>
+        </Card>
+      )}
+    </Fragment>
   );
 };
 
