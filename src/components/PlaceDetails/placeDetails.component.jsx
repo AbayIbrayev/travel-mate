b5401763d66fbef66869e0d1c2015f7b8caa7854
Abay Ibrayev <abayibrayev7@gmail.com>
2021-09-16 00:34:25 +0100

    minor change

--- src/components/PlaceDetails/placeDetails.component.jsx
+++ src/components/PlaceDetails/placeDetails.component.jsx
@@ -12,7 +12,7 @@ import {
 
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
-import Rating from '@material-ui/lab';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './placeDetails.styles';
 
@@ -29,6 +29,8 @@ const PlaceDetails = ({ place }) => {
     distance_string,
     web_url,
     website,
+    rating,
+    num_reviews,
   } = place;
   const classes = useStyles();
   return (
@@ -48,6 +50,12 @@ const PlaceDetails = ({ place }) => {
             <Typography gutterBottom variant='h6'>
               {name} ({distance_string})
             </Typography>
+            <Box display='flex' justifyContent='space-between' my={2}>
+              <Rating name='read-only' value={Number(rating)} readOnly />
+              <Typography component='legend'>
+                {num_reviews} review{num_reviews > 1 && 's'}
+              </Typography>
+            </Box>
             <Box display='flex' justifyContent='space-between'>
               <Typography variant='subtitle1'>Price</Typography>
               <Typography gutterBottom variant='subtitle1'>
@@ -63,7 +71,12 @@ const PlaceDetails = ({ place }) => {
             {awards
               ?.filter((item, index) => index < 3)
               .map((award) => (
-                <Box my={1} display='flex' justifyContent='space-between'>
+                <Box
+                  my={1}
+                  display='flex'
+                  justifyContent='space-between'
+                  key={award.display_name}
+                >
                   <img src={award.images.small} alt={award.display_name} />
                   <Typography variant='subtitle2' color='textSecondary'>
                     {award.display_name}
